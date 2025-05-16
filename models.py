0cd4009cab59cf80b951891247ea0b06af114289
qg7771481 <ddavidok845@gmail.com>
2025-05-16 18:04:14 +0200

    Create models.py

---
+++ models.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class Task(BaseModel):
+    title: str
+    description: Optional[str] = None
+    completed: bool = False
+
+class TaskUpdate(BaseModel):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    completed: Optional[bool] = None
